0b4a69879ea25d96e705fe49f6cfa84683bc9a98
yhuo <yhuo@umich.edu>
2022-04-04 17:52:55 -0400

    template page

--- search/search/views/index.py
+++ search/search/views/index.py
@@ -11,5 +11,12 @@ import search
 @search.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
+    query = flask.request.args.get('q')
+    weight = flask.request.args.get('w')
+    context = {
+        "show_search": True,
+        "search_results": [],
+    }
+    if query is None or weight is None:
+        context["show_search"] = False
     return flask.render_template("index.html", **context)
